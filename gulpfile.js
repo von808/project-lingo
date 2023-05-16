7843123402cf9fe6d1dfe5a233984fc35a410467
Alexey Sergeev <sergeevalexey808@gmail.com>
2023-05-17 01:08:05 +0700

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -111,8 +111,8 @@ function building() {
   return src([
     'app/css/style.min.css',
     'app/images/*.*',
-    '!app/images/*.svg',
-    'app/images/sprite.svg',
+    // '!app/images/*.svg',
+    // 'app/images/sprite.svg',
     'app/fonts/*.*',
     'app/js/main.min.js',
     'app/pages/*.html',
