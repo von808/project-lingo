a62c6d1b79449531be0de15b6271837677741e1f
Alexandr <alexandr.d.walker@gmail.com>
2023-05-19 17:43:04 +0700

    19.05 17:42

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,7 @@ function fonts() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
